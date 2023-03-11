9f62cf5d9d624d54dd7f1671414a0b222c8aa159
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-03-11 21:20:59 +0000

    suppressed analysis issues

--- src/Credfeto.Database.Dapper.Shared.Tests/Mocks/Banana.cs
+++ src/Credfeto.Database.Dapper.Shared.Tests/Mocks/Banana.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Credfeto.Database.Dapper.Shared.Tests.Mocks;
 
+[SuppressMessage(category: "SonarAnalyzer.CSharp", checkId: "S2096: Remove empty class", Justification = "Used for tests as is")]
 public sealed class Banana
 {
 }
